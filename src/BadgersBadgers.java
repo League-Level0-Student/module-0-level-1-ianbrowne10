1853cd12d16d3eee0db065e98390c193f758c6be
ianbrowne10 <31227286+ianbrowne10@users.noreply.github.com>
2017-11-13 18:32:29 -0800

    BadgersBadgers

---
+++ src/BadgersBadgers.java
@@ -0,0 +1,25 @@
+
+public class BadgersBadgers {
+public static void main(String[] args) {
+	
+	for (int i = 0; i < 3; i++) {
+	
+	
+
+	for (int b = 0; b < 10 ; b++) {
+	System.out.print("Badger, " );
+}
+System.out.println("");
+
+for (int b = 0; b < 2; b++) {	
+System.out.print("Mushroom, ");
+}	
+	System.out.println(" a");
+	
+	 
+	}
+	
+
+	
+}
+}
